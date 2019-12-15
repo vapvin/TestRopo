818640f6cefa0227ed76e453efb8f3f19f3e4731
vapvin <tpehdj12@daum.net>
2019-12-15 14:08:08 +0900

    Inital Commit

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("What The Fuck");
+
+    return 0;
+}
---
+++ main.s
@@ -0,0 +1,19 @@
+section .data
+    msg db "What the Fuck"
+
+section .text
+    global _start
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 12
+
+    syscall
+
+    mov rax, 60
+    mov rdi, 0
+
+    syscall
+    
